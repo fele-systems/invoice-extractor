9579746515b5e6edabb7c5cccd237fa3b8d2650d
fele <vinicius.kondo@protonmail.ch>
2024-03-22 19:12:46 +0000

    fix: Use authority instead of roles

--- src/main/java/com/systems/fele/users/config/SecurityConfig.java
+++ src/main/java/com/systems/fele/users/config/SecurityConfig.java
@@ -1,23 +1,33 @@
 package com.systems.fele.users.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import com.systems.fele.users.security.RestAuthenticationEntryPoint;
 import com.systems.fele.users.service.AppUserDetailsService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @Configuration
@@ -36,16 +46,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSec) throws Exception {
         httpSec.csrf(csrf -> csrf.disable())
                 .httpBasic(configurer -> configurer.authenticationEntryPoint(authenticationEntryPoint))
-                .authorizeHttpRequests(this::configureAuthorizeHttpRequests);
+                .authorizeHttpRequests(this::configureAuthorizeHttpRequests)
+                .exceptionHandling(this::configureExceptionHandling);
         
         return httpSec.build();
     }
     
     private void configureAuthorizeHttpRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorize) {
         // TODO: Setup which endpoints should be open and for which roles
-        authorize.requestMatchers(HttpMethod.POST, "/rest/api/users/register").authenticated()
+        authorize.requestMatchers(HttpMethod.POST, "/rest/api/users/register").hasAuthority("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/rest/api/users/remove/*").hasAuthority("ADMIN")
             .requestMatchers("error").anonymous()
             .anyRequest().authenticated();
+    }
+
+    private void configureExceptionHandling(ExceptionHandlingConfigurer<HttpSecurity> exceptionHandling) {
+        exceptionHandling.accessDeniedHandler(accessDeniedHandler());
     }
 
     @Bean
@@ -66,6 +82,29 @@ public class SecurityConfig {
     @Bean
     AppUserDetailsService userDetailsService() {
         return appUserDetailsService;
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new AccessDeniedHandler() {
+            @Override
+            public void handle(HttpServletRequest request, HttpServletResponse response,
+                    AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                response.getWriter().println(accessDeniedException.getMessage());
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                    AuthenticationException exception) throws IOException, ServletException {
+                
+            }
+        };
     }
     
     @Bean
